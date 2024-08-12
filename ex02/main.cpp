38fb4734b375ec7330f893101af72de9a82573cf
unow0517 <unow1992@gmail.com>
2024-08-12 18:28:57 +0200

    ex02 done

---
+++ ex02/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yowoo <yowoo@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/12 17:48:49 by yowoo             #+#    #+#             */
+/*   Updated: 2024/08/12 18:20:50 by yowoo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int main() 
+{
+	std::string str = "HI THIS IS BRAIN";
+	std::string *stringPTR = &str;
+	std::string &stringREF = str;
+	
+	std::cout << "The memory address of the string variable : " << &str << std::endl;
+	std::cout << "The memory address held by stringPTR      : " << stringPTR << std::endl;
+	std::cout << "The memory address held by stringREF      : " << &stringREF << std::endl;
+	
+	std::cout << "The value of the string variable : " << str << std::endl;
+	std::cout << "The value pointed by stringPTR   : " << *stringPTR << std::endl;
+	std::cout << "The value pointed by stringREF   : " << stringREF << std::endl;
+}
